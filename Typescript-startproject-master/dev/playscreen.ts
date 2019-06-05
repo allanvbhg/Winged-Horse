a52fa963f4fc0c6e886b3088d62b4f5c1dbf6c0f
jesseduijndam <50326150+jesseduijndam@users.noreply.github.com>
2019-06-05 11:23:18 +0200

    added fight cene
    did it foutje met score weet niet of het aan mij ligt

--- Typescript-startproject-master/dev/playscreen.ts
+++ Typescript-startproject-master/dev/playscreen.ts
@@ -4,15 +4,42 @@ class playscreen {
 
     private game : Game
     private nextGame: HTMLElement 
+    player : Player
+    dragon : Dragon
    
     constructor( g: Game ) {
 
         this.game = g
-
-        this.nextGame = document.createElement("nextGame")
+        let background = document.createElement("backdrak")
+        document.body.appendChild(background)
+        this.dragon = new Dragon(900 , 430, 2)
+        this.player = new Player(150 , 400, 2, this, this.game)
+        /*this.nextGame = document.createElement("nextGame")
         document.body.appendChild(this.nextGame)
-        this.nextGame.addEventListener("click", () => this.naarDeShop());
+        this.nextGame.addEventListener("click", () => this.naarDeShop());*/
     }
+
+    run(){
+        if (this.player.canrun == true) {
+          console.log("run");
+          if (this.game.score >= 50) {
+              this.game.score -= 50
+              this.naarDeShop()
+          }  
+        }       
+     }
+ 
+     die(){
+         if (this.player.die == false) {
+             this.player.canrun = false
+             console.log("ik ben dood");
+             this.dragon.delete()
+             this.player.delete()
+             let eyes = new Eyes(450, 150, 1)
+         }
+         
+         
+     }
 
     public naarDeShop(){
         this.game.shopscreen()  
